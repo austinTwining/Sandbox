b9d446e4e849df39aa7951635c23f46e9a8f7779
Austin-James Twining <austin.twining@gmail.com>
2019-06-16 18:14:55 -0400

    initial project setup

---
+++ Android/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "de3f62c8-e3e0-4730-b6a2-1ee0d5592991",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "System.Net.Http.HttpClientHandler, System.Net.Http",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep",
+
+	};
+}
---
+++ iOS/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace Sandbox
+{
+    [Register("AppDelegate")]
+    class Program : UIApplicationDelegate
+    {
+        private static GameCore game;
+
+        internal static void RunGame()
+        {
+            game = new GameCore();
+            game.Run();
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+
+        public override void FinishedLaunching(UIApplication app)
+        {
+            RunGame();
+        }
+    }
+}
