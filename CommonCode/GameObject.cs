89d4cd26c797b9faaeebad7dcacbb26da265eabb
Austin-James Twining <austin.twining@gmail.com>
2019-06-21 23:32:00 -0400

    added GameObject class

---
+++ CommonCode/GameObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Sandbox
+{
+    public class GameObject
+    {
+        public Texture2D texture;
+        public Vector2 position;
+    }
+}
